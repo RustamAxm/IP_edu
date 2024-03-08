134bc044836ad0e0522610692cb51654dee39372
rustam <rustamaxm@gmail.com>
2024-03-08 22:46:35 +0300

    to std 20

--- raw_socket/raw.cpp
+++ raw_socket/raw.cpp
@@ -4,6 +4,8 @@
 #include<netinet/ip.h>
 #include<sys/socket.h>
 #include<arpa/inet.h>
+#include <iostream>
+
 
 class RawSocket {
 public:
@@ -18,31 +20,42 @@ public:
         buffer = (unsigned char *)malloc(65536);
     }
 
-    struct iphdr * getPacket () {
+    struct iphdr & getPacket () {
         int packet_size;
         packet_size = recvfrom(sock , buffer , 65536 , 0 , NULL, NULL);
         if (packet_size == -1) {
             printf("Failed to get packets\n");
-            return nullptr;
         }
 
-        struct iphdr *ip_packet = (struct iphdr *)buffer;
-        return ip_packet;
+        struct iphdr * ip_packet = reinterpret_cast<iphdr *>(buffer);
+        return *ip_packet;
+    }
+
+    struct sockaddr_in getSourceSocket(struct iphdr & ip_packet) {
+        struct sockaddr_in source_socket_address;
+        memset(&source_socket_address, 0, sizeof(source_socket_address));
+        source_socket_address.sin_addr.s_addr = ip_packet.saddr;
+        return source_socket_address;
+    }
+
+    struct sockaddr_in getDestSocket(struct iphdr & ip_packet) {
+        struct sockaddr_in dest_socket_address;
+        memset(&dest_socket_address, 0, sizeof(dest_socket_address));
+        dest_socket_address.sin_addr.s_addr = ip_packet.daddr;
+        return dest_socket_address;
     }
 
     void printStr () {
-        struct iphdr * ip_packet = getPacket();
-        struct sockaddr_in source_socket_address, dest_socket_address;
-        memset(&source_socket_address, 0, sizeof(source_socket_address));
-        source_socket_address.sin_addr.s_addr = ip_packet->saddr;
-        memset(&dest_socket_address, 0, sizeof(dest_socket_address));
-        dest_socket_address.sin_addr.s_addr = ip_packet->daddr;
+        struct iphdr & ip_packet = getPacket();
+        auto source_socket_address = getSourceSocket(ip_packet);
+        auto dest_socket_address = getDestSocket(ip_packet);
 
+//        std::cout << "Incoming Packet: \n" << std::endl;
         printf("Incoming Packet: \n");
-        printf("Packet Size (bytes): %d\n",ntohs(ip_packet->tot_len));
-        printf("Source Address: %s\n", (char *)inet_ntoa(source_socket_address.sin_addr));
-        printf("Destination Address: %s\n", (char *)inet_ntoa(dest_socket_address.sin_addr));
-        printf("Identification: %d\n\n", ntohs(ip_packet->id));
+        printf("Packet Size (bytes): %d\n",ntohs(ip_packet.tot_len));
+        printf("Source Address: %s\n", reinterpret_cast<char *>(inet_ntoa(source_socket_address.sin_addr)));
+        printf("Destination Address: %s\n", reinterpret_cast<char *>(inet_ntoa(dest_socket_address.sin_addr)));
+        printf("Identification: %d\n\n", ntohs(ip_packet.id));
     }
 
 private:
